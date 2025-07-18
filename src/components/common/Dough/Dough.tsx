586eb5ddb1aec06f6dc57c99a2dca989c6fd7c16
Ilya Pimonov <13ilyapimonov@gmail.com>
2025-07-18 22:32:06 +0300

    creates radio button component

--- src/components/common/Dough/Dough.tsx
+++ src/components/common/Dough/Dough.tsx
@@ -1,18 +1,17 @@
 import React from 'react'
 import styles from './dough.module.css'
+import Radio from '@/components/ui/Radio/Radio'
 
 const Dough = () => {
   return (
     <div className={styles.wrapper}>
         <label htmlFor="" className={`t-lg-16`}>Тип теста:</label>
-        <div>
+        <div className={styles.container}>
             <div>
-                <input type="radio" />
-                <label htmlFor="">Традиционное</label>
+              <Radio content={'Традиционное'} />
             </div>
             <div>
-                <input type="radio" />
-                <label htmlFor="">Тонкое</label>
+              <Radio content={'Тонкое'} />
             </div>
         </div>
     </div>
